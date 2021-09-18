e99102b63d49c0a4af2621a4d7203fb0bbb66760
earvingad <arciga.duran@gmail.com>
2021-09-18 16:34:21 -0500

    edit workflow and config for x-wrt rt-acrh17

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.15.1/10.1.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168/10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168/10.1/g' package/base-files/files/bin/config_generate
