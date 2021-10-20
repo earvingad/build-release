80444fa13b863b834eee58c987ce8a4eee6d50e5
earvingad <arciga.duran@gmail.com>
2021-10-19 19:59:43 -0500

    release: 2021-10-19
    Signed-off-by: earvingad <arciga.duran@gmail.com>

--- xbuild.sh
+++ xbuild.sh
@@ -1,6 +1,7 @@
 day=$(date +%F)
-sed -i "s/name: Build x-wrt.*/name: Build x-wrt-$day/g" .github/workflows/build-xwrt.yml && \
+echo -n 9.0_b$day >release.tag && \
+# sed -i "s/name: Build x-wrt.*/name: Build x-wrt-$day/g" .github/workflows/build-xwrt.yml && \
 sed -i "s/9.0_b.*/9.0_b$day\"/" .configx && \
 git add . && \
-git commit --signoff -am "release: $(date)" && \
+git commit --signoff -am "release: $day" && \
 git push origin master || exit 1
