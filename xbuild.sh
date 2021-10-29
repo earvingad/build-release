14abfc98da0aa4fb78958ea1cbc9e39fc617cf29
earvingad <arciga.duran@gmail.com>
2021-10-29 13:27:10 -0500

    release: 2021-10-29

--- xbuild.sh
+++ xbuild.sh
@@ -3,5 +3,5 @@ echo -n 9.0_b$day >release.tag && \
 # sed -i "s/name: Build x-wrt.*/name: Build x-wrt-$day/g" .github/workflows/build-xwrt.yml && \
 sed -i "s/9.0_b.*/9.0_b$day\"/" .configx && \
 git add . && \
-git commit --signoff -am "release: $day" && \
+git commit -m "release: $day" && \
 git push origin master || exit 1
