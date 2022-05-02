df85036de4a7396bb0f58ad55e44a19618d99628
earvingad <arciga.duran@gmail.com>
2022-05-02 14:49:25 -0500

    release: 2022-05-02

--- xbuild.sh
+++ xbuild.sh
@@ -1,7 +1,7 @@
 day=$(date +%F)
-echo -n 21.10_b$day >release.tag && \
+echo -n 22.03_b$day >release.tag && \
 # sed -i "s/name: Build x-wrt.*/name: Build x-wrt-$day/g" .github/workflows/build-xwrt.yml && \
-sed -i "s/21.10_b.*/21.10_b$day\"/" .configx && \
+sed -i "s/22.03_b.*/22.03_b$day\"/" .configx && \
 git add . && \
 git commit -m "release: $day" && \
 git push origin master || exit 1
