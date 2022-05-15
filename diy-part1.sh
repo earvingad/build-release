6603f6d965c35d6bce5f44424cc7631e5a697709
earvingad <arciga.duran@gmail.com>
2022-05-15 14:57:47 -0500

    May15-2022

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,13 +17,21 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # Modify default IP
-sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/='\''OpenWrt'\''/='\''krypton'\''/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/='\''OpenWrt'\''/='\''krypton'\''/g' package/base-files/files/bin/config_generate
 
 cat << "EOF" > package/base-files/files/etc/uci-defaults/99_custom
 uci set wireless.@wifi-device[0].disabled='0'
 uci set wireless.@wifi-device[1].disabled='0'
 uci commit wireless
+
+uci set network.lan.ipaddr='10.1.1.1'
+uci commit network
+
+uci set system.@system[0].hostname='krypton'
+uci set system.@system[0].zonename='America/Mexico City'
+uci set system.@system[0].timezone='CST6CDT,M4.1.0,M10.5.0'
+uci commit system
 
 uci set fstab.@global[0].anon_mount='1'
 uci commit fstab
